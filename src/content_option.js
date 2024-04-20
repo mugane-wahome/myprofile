e54e08f04d7a0df71742238208e61f16309959df
mugane-wahome <muganewahome@gmail.com>
2024-04-20 14:05:30 +0300

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+
+import React from "react";
+import profile from "./assets/images/profile.jpg";
+
+const logotext = "MUGANE";
+const meta = {
+    title: "WAHOME MUGANE",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Mugane Wahome",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I love data analysis",
+    },
+    description: "I'm Samuel Mugane, a full-stack developer with a background in Actuarial Science. I bring a unique perspective to problem-solving and data analysis. When I'm not crafting web applications, you can find me exploring new technologies and finding creative solutions to complex challenges.",
+    your_img_url: profile,
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'm Samuel Mugane, a full-stack developer with a background in Actuarial Science. I bring a unique perspective to problem-solving and data analysis. When I'm not crafting web applications, you can find me exploring new technologies and finding creative solutions to complex challenges. My passion lies in bringing ideas to life through code and creating memorable user experiences.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Web developer intern",
+        where: "Nyeri ~ Kenya",
+        date: "2024",
+    },
+    {
+        jobtitle: "Data analyst",
+        where: "remote ~ India",
+        date: "2023",
+    },
+    {
+        jobtitle: "Peer teacher",
+        where: "kenya",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "R",
+        value: 90,
+    },
+    {
+        name: "POWER BI",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Node.js",
+        value: 60,
+
+    },
+    {
+        name: "express",
+        value: 85,
+    },
+
+    {
+        name: "mysql, mongo, postgressql",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Data Analysis",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "canva designing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: profile,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "muganewahome@gmail.com",
+    YOUR_FONE: "+254719472162",
+    description: "Hey there! If you're interested in collaborating, have a project in mind, or just want to chat, feel free to reach out to me. I'm always excited to connect with like-minded individuals and explore new opportunities. Looking forward to hearing from you! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/mugane-wahome",
+    facebook: "https://facebook.com",
+    linkedin: "www.linkedin.com/in/wahome-mugane",
+    twitter: "hhttps://x.com/MeMugane?t=xECC7sCk3MAHCrPJn4GjDA&s=09",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
